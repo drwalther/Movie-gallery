2e9d9b64121e28afe9a7d0a7396807d0a9bf2d3b
Rakhaev Artem <a.u.rahaev@gmail.com>
2023-03-15 21:39:35 +0300

    added header and footer components

---
+++ src/layout/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer () {
+	return <footer className="page-footer grey">	
+	<div className="footer-copyright">
+	  <div className="container">
+	  © {new Date().getFullYear()} Coded by Rakhaev
+	  <a className="grey-text text-lighten-4 right" href="#!">Contact Us</a>
+	  </div>
+	</div>
+  </footer>
+}
+
+export {Footer};
---
+++ src/layout/Main.jsx
@@ -0,0 +1,5 @@
+function Main () {
+	return <main>Hello</main>
+}
+
+export {Main};
