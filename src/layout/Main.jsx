8e4270682f5b48762c443ccd020331882c56c2a6
Rakhaev Artem <a.u.rahaev@gmail.com>
2023-03-21 22:40:39 +0300

    added movies cards drawing

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,5 +1,24 @@
-function Main () {
-	return <main className="container content">Hi</main>
+import React from "react";
+import {Movies} from "../components/Movies";
+
+class Main extends React.Component {
+
+	state = {
+		movies: []
+	}
+
+	render () {
+		const {movies} = this.state;
+
+		return <main className="container content"> 
+		{
+			movies.length ? (
+			<Movies movies={this.state.movies}/>
+			) : <h3>Loading</h3>
+		}
+			
+		</main>
+	}	
 }
 
 export {Main};
